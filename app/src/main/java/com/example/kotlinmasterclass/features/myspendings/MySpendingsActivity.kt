4c632802210e8531eb1bf9631f0a5be5dffa69f8
VivekThummar52 <vivekthummar52@gmail.com>
2026-06-16 13:32:35 +0530

    Improve layout in MySpendingsActivity - Added `navigationBarsPadding()` to the bottom container to ensure proper spacing from the system navigation bar.

--- app/src/main/java/com/example/kotlinmasterclass/features/myspendings/MySpendingsActivity.kt
+++ app/src/main/java/com/example/kotlinmasterclass/features/myspendings/MySpendingsActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -473,6 +474,7 @@ fun MySpendingsBottomNavigation(
     Box(
         modifier = Modifier
             .fillMaxWidth()
+            .navigationBarsPadding()
             .height(72.dp)
     ) {
         Canvas(
